df924bd362d1d3611418c11c36ea7f0eb450cbb9
f20140868@goa.bits-pilani.ac.in <shivam.agarwal345@gmail.com>
2017-11-24 23:53:53 +0530

    2nd

--- forwarding.v
+++ forwarding.v
@@ -1,47 +1,136 @@
-module forwarding(input p2_regwr1, input p2_regwr2, input [4:0] p0_rs1,input [4:0] p0_rs2,input [4:0] p0_rsc,input p3_regwr, input p3_regwrc,input [4:0] p3_regdest,input [4:0] p3_regdestc, input p3_memwr, input [4:0] p2_regdest,input [4:0] p2_regdestc,output reg [2:0]fwd1, output reg [2:0] fwd2);
-	always@(p2_regwr1, p2_regwr2, p0_rs1, p0_rs2, p0_rsc, p3_regwr1, p3_regwr2, p3_regdest, p3_regdestc, p3_memwr, p2_regdest, p2_regdestc,  
+module forwarding(input [6:0] opcode,input[1:0] opcodec,input[2:0] funct,input p2_regwr1, input p2_regwr2, input [4:0] p0_rs1,input [4:0] p0_rs2,input [4:0] p0_rsc,input p3_regwr, input p3_regwrc,input [4:0] p3_regdest,input [4:0] p3_regdestc, input p3_memwr, input [4:0] p2_regdest,input [4:0] p2_regdestc,output reg [2:0]fwd1, output reg [2:0] fwd2, output reg [2:0] fwd3, output reg[2:0] fwd4, output [2:0] fwd5);
+	always@(opcode, opcodec,  funct, p2_regwr1, p2_regwr2, p0_rs1, p0_rs2, p0_rsc, p3_regwr1, p3_regwr2, p3_regdest, p3_regdestc, p3_memwr, p2_regdest, p2_regdestc,  
 	p2_memwr)
 	begin
 
+			// p0_rs1 is rs of current instruction p0_rsc is rs of compressed instruction
+			// regwr2 anything2 means that of compressed instruction
+			// p0 means current p1 is pipeline1 or basically instruction before the current1  and so on
+			// since the instruction before this must have been a flush one if there was a hazard we only need to consider p2 and p3
+			// 0000.. will be passed as opcode when this module is used in execute cycle to detect that we are currently not calling it for a branch or jump
+			// branch and jump are considered the only instructions executing in ID cycle as of now
 
 		if(p2_regwr==1'b1 && p2_regdest!=5'b0 && p2_regdest == p0_rs1)
-		fwd1 = 3'b001;
+		
+		begin
+		if(opcode == 7'b0000000)			
+		fwd1 =  3'b001;
+		if(opcode == 7'b1100111)			// 1100111 is opcode of jump
+		fwd3 = 3'b001;
+		end
+		
 		else if (p2_regwrc==1'b1 && p2_regdestc!=5'b0 && p2_regdestc == p0_rs1)
-		fwd1 = 3'b010;
+		
+		begin
+		if(opcode == 7'b0000000)
+		fwd1 =  3'b010;
+		if(opcode == 7'b1100111)			// 1100111 is opcode of jump
+		fwd3 = 3'b010;
+		end
+
 		else if (p3_regwr==1'b1 && p3_regdest!=5'b0 && p3_regdest == p0_rs1)
-		fwd1 = 3'b011;
+		
+		begin
+		if(opcode == 7'b0000000)
+		fwd1 =  3'b011;
+		if(opcode == 7'b1100111)			// 1100111 is opcode of jump
+		fwd3 = 3'b011;
+		end
+
 		else if (p3_memwr==1'b1 && p3_regdest!=5'b0 && p3_regdest == p0_rs1)
-		fwd1 = 3'b011;	
+		
+		begin
+		if(opcode == 7'b0000000)
+		fwd1 =  3'b011;
+		if(opcode == 7'b1100111)			// 1100111 is opcode of jump
+		fwd3 = 3'b011;
+		end
+
 		else if (p3_regwrc==1'b1 && p3_regdest!=5'b0 && p3_regdestc == p0_rs1)
-		fwd1 = 3'b100;
+		
+		begin
+		if(opcode == 7'b0000000)
+		fwd1 =  3'b100;
+		if(opcode == 7'b1100111)			// 1100111 is opcode of jump
+		fwd3 = 3'b100;
+		end
+
 		else
-		fwd1 = 3'b000;
+		begin
+		if(opcode == 7'b0000000)
+		fwd1 =  3'b000;
+		if (opcode == 7'b1100111)
+		fwd3 = 3'b000;
+		end
+	
 
 		if(p2_regwr==1'b1 && p2_regdest!=5'b0 && p2_regdest == p0_rs2)
+		begin
+		if(opcode == 7'b0000000)
 		fwd2 = 3'b001;
+		end
 		else if (p2_regwrc==1'b1 && p2_regdestc!=5'b0 && p2_regdestc == p0_rs2)
+		begin
+		if(opcode == 7'b0000000)
 		fwd2 = 3'b010;
+		end
 		else if (p3_regwr==1'b1 && p3_regdest!=5'b0 && p3_regdest == p0_rs2)
+		begin
+		if(opcode == 7'b0000000)
 		fwd2 = 3'b011;
+		end
 		else if (p3_memwr==1'b1 && p3_regdest!=5'b0 && p3_regdest == p0_rs2)
-		fwd2 = 3'b011;	
+		begin
+		if(opcode == 7'b0000000)
+		fwd2 = 3'b011;
+		end
 		else if (p3_regwrc==1'b1 && p3_regdest!=5'b0 && p3_regdestc == p0_rs2)
+		begin
+		if(opcode == 7'b0000000)
 		fwd2 = 3'b100;
+		end
 		else
+		begin
+		if(opcode == 7'b0000000)
 		fwd2 = 3'b000;
+		end
+
 
 		if(p2_regwr==1'b1 && p2_regdest!=5'b0 && p2_regdest == p0_rsc)
+		begin
+		if(opcode ==2'b00 && funct == 3'b000)
 		fwd3 = 3'b001;
+																	// means branch instruction
+		if(opcode ==2'b00 && funct == 3'b000)
+		fwd5 = 3'b001;
+		end
 		else if (p2_regwrc==1'b1 && p2_regdestc!=5'b0 && p2_regdestc == p0_rsc)
+		begin
+		if(opcode ==2'b00 && funct == 3'b000)
 		fwd3 = 3'b010;
+		if (opcode1 == 2'b01 && funct == 3'b111)				// means branch instruction
+		fwd5 = 3'b010;
+		end
 		else if (p3_regwr==1'b1 && p3_regdest!=5'b0 && p3_regdest == p0_rsc)
+		begin
+		if(opcode ==2'b00 && funct == 3'b000)
 		fwd3 = 3'b011;
+		if (opcode1 == 2'b01 && funct == 3'b111)				// means branch instruction
+		fwd5 = 3'b011;
+		end
 		else if (p3_memwr==1'b1 && p3_regdest!=5'b0 && p3_regdest == p0_rsc)
-		fwd3 = 3'b011;	
-		else if (p3_regwrc==1'b1 && p3_regdest!=5'b0 && p3_regdestc == p0_rsc)
+		begin
+		if(opcode ==2'b00 && funct == 3'b000)
+		fwd3 = 3'b011;
+		if (opcode1 == 2'b01 && funct == 3'b111)				// means branch instruction
+		fwd5 = 3'b011;
+		end
+		else (p3_regwrc==1'b1 && p3_regdest!=5'b0 && p3_regdestc == p0_rsc)
+		begin
+		if(opcode ==2'b00 && funct == 3'b000)
 		fwd3 = 3'b100;
-		else
+		if (opcode1 == 2'b01 && funct == 3'b111)
 		fwd3 = 3'b000;
-
+		end
 	end	
 endmodule
--- hazard.v
+++ hazard.v
@@ -1,7 +1,8 @@
-module hazard( input p1_memread, input p1_regwrite, p1_regdestc, input src1, input src2, input src3,
+module hazard( input opcode[1:0], input [2:0] funct,input p1_memread, input p1_regwrite, p1_regdestc,input p2_memread, input p2_regwrite, p2_regdestc input src1, input src2, input src3,
 			output reg id_ex_controlvalue, output reg pc_write, output reg if_id_write);
-	always @ (p1_memread or p1_regwrite or src1 or src2 or src3)
+	always @ (p1_memread or p1_regwrite or src1 or src2 or src3 or p2_memread or p2_regwrite or p1_regdest or p1_regdestc or p2_regdestc or p2_regdestc )
 	begin
+            // Load use hazard
 			if (p1_memread == 1'b1 && p1_regwrite == 1'b1 && (src1 == p1_regdestc || src2 == p1_regdest || src3 == p1_regdest)
 			  begin
                 id_ex_control_value=1;
@@ -15,5 +16,27 @@ module hazard( input p1_memread, input p1_regwrite, p1_regdestc, input src1, inp
                 pc_write=1;
                 if_id_write=1;
             end  
-	end
+	
+            // beq hazard
+            if(opcode[1:0]==2'b01 && funct == 3'b111)               // opcode for branch
+            begin
+            if(p1_memwread == 1'b1 && p1_regwrite == 1'b1 && (src1 == p1_regdestc || src2 == p1_regdest || src3 == p1_regdest))
+               // have to intoduce two stalls
+
+            if (p1_regwrite == 1'b1 && (src1 == p1_regdestc || src2 == p1_regdest || src3 == p1_regdest))
+                  begin
+                id_ex_control_value=1;
+                pc_write=0;
+                if_id_write=0;                                      // single stall
+            end  
+
+            if (p2_memread == 1'b1 && p2_regwrite == 1'b1 && (src1 == p2_regdestc || src2 == p2_regdestc || src3 == p2_regdestc))
+             begin
+                id_ex_control_value=1;
+                pc_write=0;
+                if_id_write=0;                                      // single stall
+            end
+            end
+    end
+
 endmodule
