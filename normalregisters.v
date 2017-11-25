feb9c48d77d252ff0ac85b26574600baaa2186f7
f20140868@goa.bits-pilani.ac.in <shivam.agarwal345@gmail.com>
2017-11-25 19:33:54 +0530

    5th

--- normalregisters.v
+++ normalregisters.v
@@ -1,4 +1,4 @@
-//Normal Registers begin
+/Normal Registers begin
 module D_ff_n(input clk, input reset, input regWrite, input decOut1b, input d, output reg q);
 	always @ (negedge clk)
 	begin
@@ -20,6 +20,56 @@ module D_ff_n_1(input clk, input reset, input flush, input regWrite, input decOu
 	end
 endmodule
 
+module register48bit_n_1( input clk, input reset, input flush, input regWrite, input decOut1b, input [47:0] writeData, output  [47:0] outR );
+	D_ff_n_1 d0(clk, reset, flush, regWrite, decOut1b, writeData[0], outR[0]);
+	D_ff_n_1 d1(clk, reset, flush, regWrite, decOut1b, writeData[1], outR[1]);
+	D_ff_n_1 d2(clk, reset, flush, regWrite, decOut1b, writeData[2], outR[2]);
+	D_ff_n_1 d3(clk, reset, flush, regWrite, decOut1b, writeData[3], outR[3]);
+	D_ff_n_1 d4(clk, reset, flush, regWrite, decOut1b, writeData[4], outR[4]);
+	D_ff_n_1 d5(clk, reset, flush, regWrite, decOut1b, writeData[5], outR[5]);
+	D_ff_n_1 d6(clk, reset, flush, regWrite, decOut1b, writeData[6], outR[6]);
+	D_ff_n_1 d7(clk, reset, flush, regWrite, decOut1b, writeData[7], outR[7]);
+	D_ff_n_1 d8(clk, reset, flush, regWrite, decOut1b, writeData[8], outR[8]);
+	D_ff_n_1 d9(clk, reset, flush, regWrite, decOut1b, writeData[9], outR[9]);
+	D_ff_n_1 d10(clk, reset, flush, regWrite, decOut1b, writeData[10], outR[10]);
+	D_ff_n_1 d11(clk, reset, flush, regWrite, decOut1b, writeData[11], outR[11]);
+	D_ff_n_1 d12(clk, reset, flush, regWrite, decOut1b, writeData[12], outR[12]);
+	D_ff_n_1 d13(clk, reset, flush, regWrite, decOut1b, writeData[13], outR[13]);
+	D_ff_n_1 d14(clk, reset, flush, regWrite, decOut1b, writeData[14], outR[14]);
+	D_ff_n_1 d15(clk, reset, flush, regWrite, decOut1b, writeData[15], outR[15]);
+	D_ff_n_1 d16(clk, reset, flush, regWrite, decOut1b, writeData[16], outR[16]);
+	D_ff_n_1 d17(clk, reset, flush, regWrite, decOut1b, writeData[17], outR[17]);
+	D_ff_n_1 d18(clk, reset, flush, regWrite, decOut1b, writeData[18], outR[18]);
+	D_ff_n_1 d19(clk, reset, flush, regWrite, decOut1b, writeData[19], outR[19]);
+	D_ff_n_1 d20(clk, reset, flush, regWrite, decOut1b, writeData[20], outR[20]);
+	D_ff_n_1 d21(clk, reset, flush, regWrite, decOut1b, writeData[21], outR[21]);
+	D_ff_n_1 d22(clk, reset, flush, regWrite, decOut1b, writeData[22], outR[22]);
+	D_ff_n_1 d23(clk, reset, flush, regWrite, decOut1b, writeData[23], outR[23]);
+	D_ff_n_1 d24(clk, reset, flush, regWrite, decOut1b, writeData[24], outR[24]);
+	D_ff_n_1 d25(clk, reset, flush, regWrite, decOut1b, writeData[25], outR[25]);
+	D_ff_n_1 d26(clk, reset, flush, regWrite, decOut1b, writeData[26], outR[26]);
+	D_ff_n_1 d27(clk, reset, flush, regWrite, decOut1b, writeData[27], outR[27]);
+	D_ff_n_1 d28(clk, reset, flush, regWrite, decOut1b, writeData[28], outR[28]);
+	D_ff_n_1 d29(clk, reset, flush, regWrite, decOut1b, writeData[29], outR[29]);
+	D_ff_n_1 d30(clk, reset, flush, regWrite, decOut1b, writeData[30], outR[30]);
+	D_ff_n_1 d31(clk, reset, flush, regWrite, decOut1b, writeData[31], outR[31]);
+	D_ff_n_1 d32(clk, reset, flush, regWrite, decOut1b, writeData[32], outR[32]);
+	D_ff_n_1 d33(clk, reset, flush, regWrite, decOut1b, writeData[33], outR[33]);
+	D_ff_n_1 d34(clk, reset, flush, regWrite, decOut1b, writeData[34], outR[34]);
+	D_ff_n_1 d35(clk, reset, flush, regWrite, decOut1b, writeData[35], outR[35]);
+	D_ff_n_1 d36(clk, reset, flush, regWrite, decOut1b, writeData[36], outR[36]);
+	D_ff_n_1 d37(clk, reset, flush, regWrite, decOut1b, writeData[37], outR[37]);
+	D_ff_n_1 d38(clk, reset, flush, regWrite, decOut1b, writeData[38], outR[38]);
+	D_ff_n_1 d39(clk, reset, flush, regWrite, decOut1b, writeData[39], outR[39]);
+	D_ff_n_1 d40(clk, reset, flush, regWrite, decOut1b, writeData[40], outR[40]);
+	D_ff_n_1 d41(clk, reset, flush, regWrite, decOut1b, writeData[41], outR[41]);
+	D_ff_n_1 d42(clk, reset, flush, regWrite, decOut1b, writeData[42], outR[42]);
+	D_ff_n_1 d43(clk, reset, flush, regWrite, decOut1b, writeData[43], outR[43]);
+	D_ff_n_1 d44(clk, reset, flush, regWrite, decOut1b, writeData[44], outR[44]);
+	D_ff_n_1 d45(clk, reset, flush, regWrite, decOut1b, writeData[45], outR[45]);
+	D_ff_n_1 d46(clk, reset, flush, regWrite, decOut1b, writeData[46], outR[46]);
+	D_ff_n_1 d47(clk, reset, flush, regWrite, decOut1b, writeData[47], outR[47]);
+endmodule
 //Register 32 bit
 module register32bit_n( input clk, input reset, input regWrite, input decOut1b, input [31:0] writeData, output  [31:0] outR );
 	D_ff_n d0(clk, reset, regWrite, decOut1b, writeData[0], outR[0]);
@@ -106,4 +156,3 @@ module register3bit( input clk, input reset, input regWrite, input decOut1b, inp
 	D_ff_n d1(clk, reset, regWrite, decOut1b, writeData[1], outR[1]);
 	D_ff_n d2(clk, reset, regWrite, decOut1b, writeData[2], outR[2]);
 endmodule
-
--- pipeline.v
+++ pipeline.v
@@ -1,3 +1,4 @@
+
 module pipeline(input clk, input reset, output [31:0] Result1, output [31:0] Result2);
 wire PCWrite, p0_Write, IF_Flush, p3_regWrite1, p3_regWrite2, p3_negativeFlag1, reg_negativeFlag, p3_overflowFlag, reg_overflowFlag, p3_carryFlag, reg_carryFlag;
 wire p3_zeroFlag1, reg_zeroFlag, p2_overflowFlag, memRead, memWrite, regWrite1, regWrite2, CauseWrite, EPC_Write, ID_Flush, EX_Flush, p1_negativeFlag, p1_carryFlag, p1_overflowFlag, p1_zeroFlag;
@@ -12,131 +13,131 @@ wire [2:0] p3_rd1, p3_rd2, p1_rm1, p1_rn1, p1_rm2, p1_rn2, p1_rd1, p1_rd2, p2_rd
 wire [11:0] jump_pc;
 wire [1:0] aluSrcB, aluOp, PCSource, p1_aluSrcB, p1_aluOp, aluSrcB_mux, aluOp_mux, ForwardA1, ForwardA2, ForwardB1, ForwardB2, ForwardC2;
 wire [4:0] p1_opcode1, p1_opcode2;
-register32bit pc_Reg(clk, reset, PCWrite, 1'b1, PC_MuxOut, PC_Out);
-adder32 pc_Adder(PC_Out, 32'd4, PC_AdderOut);
-mux4to1_32bits1 mux(PC_AdderOut,{p0_pc[31:12],jump_pc}, branch_pc, 32'd60, PCSource, PC_MuxOut);
-Mem IM_Call(clk, reset, 1'b0, 1'b1, PC_Out, 32'd0, instr1, instr2);
 
-mux4to1_32bit mux_pc(pc_adderOut, PC1, PC2, 32'd60, pc_in_sel, pc_final); 
-register32bit_n PC( clk, reset, pc_write, 1'b1, pc_final, pcOut );
+or obj (pcwrite, jump, branchcomparator);
+mux2to1_32bit mux_pc(pc_adderOut,branch_pc, pcwrite, PC_final);     
+//PC1 represents branch_pc this will be true if branch is taken
+  
+register32bit_n PC( clk, reset, PC_write, 1'b1, PC_final, pcOut );
+  
+adder PC_add(pcOut, 32'b0000_0000_0000_0000_0000_0000_0000_0100, pc_adderOut);
+  
+Mem IM( clk, reset, 1'b0, 1'b1, pcOut, 32'b0000_0000_0000_0000_0000_0000_0000_0000, IR );
+  
+IF_ID p0_call(clk, reset, IF_ID_write, IF_Flush, 1'b1, PC_AdderOut, IR, p0_pc, p0_instr); 
 
-IF_ID p0_call(clk, reset, IF_ID_write, IF_Flush, 1'b1, PC_AdderOut, instr1, instr2, p0_instr1, p0_instr2, p0_pc); 
+
+signExt5to32 s1 (p0_instr1[40:36], shift_amt_32);
+signExt12to32 s2 (p0_instr1[47:36], sign_ext_12_Itype);
+signExt13to32 s3 ({p0_instr1[47:36], 1'b0}, sign_ext_13);
+signExt12to32 s4 ({p0_instr[47:41], p0_instr1[27:23]}, sign_ext_12_Stype);
+signExt18to32 s5 ({{p0_instr[12], p0_instr[6:2]}, 12'b0}, sign_ext_18);
+signExt8to32 s6 ({p0_instr[12],p0_instr[6:5],p0_instr[2],p0_instr[11:10], p0_instr[4:3]}, branch_address);
+// regdestcompressed to be decided by a mux during execute
+registerFile rf_call(clk, reset, p3_regwrite1, p3_regwrite2, p0_instr1[35:31], p0_instr1[40:36], p0_instr2[9:7], p0_instr[27:23], ??,  p3_memout,
+  p3_aluout, p0_rs1, p0_rs2, p0_rsc); 
+
+wire [31:0] out1, out2;
+mux8to1_32bit Forward1  (p0_pc, p2_aluout, p2_adderout, regdata, regdatac, forward5, out_temp1);
 
 
-signExt5to32 s1 (p0_instr1[24:20], shift_amt_32);
-signExt12to32 s2 (p0_instr1[31:20], sign_ext_12_Itype);
-signExt13to32 s3 ({p0_instr1[31:20], 1'b0}, sign_ext_13);
-signExt12to32 s4 ({p0_instr1[31:25], p0_instr1[11:7]}, sign_ext_12_Stype);
-signExt18to32 s5 ({{p0_instr2[12], p0_instr2[6:2]}, 12'b0}, sign_ext_18);
-signExt8to32 s6 ({p0_instr2[12],p0_instr2[6:5],p0_instr2[2],p0_instr2[11:10], p0_instr2[4:3]}, branch_address);
+mux8to1_32bit Forward1  (p0_reg_rs1, p2_aluout, p2_adderout, regdata, regdatac, forward4, out_temp2);   
 
-registerFile rf_call(clk, reset, p3_regWrite1, p3_regWrite2, p0_instr1[19:15], p0_instr1[24:20], p0_instr2[9:7], p0_instr2[8:6], p0_instr2[5:3], p0_instr2[2:0],
-	p3_rd2, Result1, Result2, reg_rm1, p0_reg_rs1, p0_reg_rs2, p0_reg_rs_c, p0_reg_rn2, p0_reg_rd2);	//yso much shit here
+mux2to1_32bit IDaddersource1 (out_temp1,out_temp2,jump, out1);  
 
-adder32 branch_adder(adder_branch_input, p0_pc, branch_pc);
+mux2to1_32bit IDaddersource2 (branch_address, sign_ext_13, jump, out2);
+ 
+adder32 branch_adder(out1, out2, branch_pc);
 
 comparator c1 (p0_rsc, 32'b0, flag); // flag = 1'b1 means result of comaprator was true
 
-and a (to_pc_se_pehle_mux, branch, branch_comparator); // branch comes from control ckt this signal is just for seeing if branch is required and ifflush will be made = this signal
-
+and a (flag, branch, branch_comparator); // branch comes from control ckt this signal is just for seeing if branch is required and ifflush will be made = this signal
+// branch_comparator should be one for branch to be taken
 
   ctrlCkt Ctrlckt( p0_intr[22:16], p0_instr[30:28], pc_in, regwrite,  
-  jump, aluOp, memwrite, IF_Flush, regread, memtoreg, aluSrcb, slti);	// overflow flag not kept add_shoft_bar not kept epcwrite1 causewrite2 exception regread added aluSrcb added
-
-  ctrlCktcompressed Ctrlckr (p0_instr[1:0], p0_instr[15:13], pc_inc regwritec, branchc, memread, IF_Flushc, regread, memtoregc, addersrcA, addersrcB, regdestc ) //adderssrcA and B added  pc_incompressed added
+  jump, aluOp, memwrite, IF_Flush, regread, memtoreg, aluSrcb, slti); // overflow flag not kept add_shoft_bar not kept epcwrite1 causewrite2 exception regread added aluSrcb added
+  
+  ctrlCktcompressed Ctrlckr (p0_instr[1:0], p0_instr[15:13], pc_inc, regwritec, branch, memread, IF_Flushc, regread, memtoregc, regdestc); //adderssrcA and B added  pc_incompressed added
 
 
    mux2to1_1bit ctrl_mux_1(regwrite, 1'b0, hd_stall , mux_regwrite);
- 	mux2to1_1bit ctrl_mux_2(pcwrite, 1'b0, hd_stall , mux_pcwrite);
+  mux2to1_1bit ctrl_mux_2(pcwrite, 1'b0, hd_stall , mux_pcwrite);
    mux2to1_1bit ctrl_mux_3(memwrite, 1'b0, hd_stall , mux_memwrite);
    mux2to1_1bit ctrl_mux_4(regwritec, 1'b0, hd_stall , mux_regwritec);
-   mux2to1_1bit ctrl_mux_5(pcwrite_c, 1'b0, hd_stall , mux_regwritec);
-
-    mux2to1_32bit branch_mux(p0_pc, bvalue, branch_comparator , PC1); 
-     
- 
-ID_EX p1_call(clk,reset, 1'b1, 1'b1, all control signals,p0_pc, p1_pc, topcsepehlemux, p0_rs1, p1_rs1, p0_rs2, p1_rs2, p0_rs3, p1_rs3, p0_instr[27:23], p1_regdest, p1_instr[11:7], p1_regdestc1, p0_instr[4:2], p1_regdestc2,shift_amt_32,p1_shift_amt_32, sign_ext_12_Itype,p1_sign_ext_12_Itype, sign_ext_13,p1_sign_ext_13,sign_ext_12_Stype,p1_sign_ext_12_Stype,sign_ext_18,p1_sign_ext_18,p0_instr[35:31],p0_instr[40:36],p1_instr_rs1,p1_instr_rs2,p0_instr[4:2], p1_instr_rsc, branch_address, p1_branch_address, {15'b0,p0_instr[6:2],2'b0}, p1_concat, p1_to_pc_se_pehle_mux);			//instr_rs means the actual bits not reg(rs)
-
-
-mux8to1 alusrca (p1_rs1, p3_aluout, p3_adderout, p2_aluout, p2_adderout, wb_out, wb_outc, alusrca, 32'b0, aluIn1); 
-
-mux8to1 m1 (p1_rs2, p3_aluout, p3_adderout, p2_aluout, p2_adderout, wb_out, wb_outc, alusrca, 32'b0, temp); 
-assign p1_rs2 = temp;
-mux4to1 m2 	(temp, p1_shift_amt_32,p1_sign_ext_12_Itype, p1_sign_ext_13, p1_sign_ext_12_Stype, 32'b0, 32'b0, 32'b0, signal, aluIn2);
-
-
-mux8to1_32bits1 Forward1  (p1_rs1, p2_adderout, p2_adderout, p3_someout, p3_someout1, alu_in1);		// some_out1 some_out2
-mux8to1_32bits1 Forward2  (p1_rs2, p2_adderout, p2_adderout, p3_someout, p3_someout1, alu_in2_temp);
-mux8to1_32bits1 Forward3  (p1_rsc, p2_adderout, p2_adderout, p3_someout, p3_someout1, adder_in1_temp);
-mux2to1_32bits  obj        (adder_in1_temp, p1_pc, branchc, adder_in1);
-mux8to1_32bits  lastmuxbeforealu (alu_in2_temp, p1_shift_amt_32, sign_ext_12_Itype, p1_sign_ext_12_Stype, p1_sign_ext_13, 32'b0, 32'b0, 32'b0, alu_in2);
-mux2to1_32bits  obj1       (p1_branch_address, p1_concat, adder_in2);			// the last mux before adder uses branch address and CLW instruction ka 2nd input
+   mux2to1_1bit ctrl_mux_5(pcwrite_c, 1'b0, hd_stall , mux_pcwritec);
 
 
 
+ID_EX call (clk, reset, 1'b1, 1'b1, all control signals, p0_pc, p0_instr[35:31], p0_instr[40:36], p0_rs1, p0_rs2, p0_rsc, 
+{2'b00, p0_instr[9:7]}, p0_instr[27:23], p0_instr[11:7], {2'b00,p0_instr[4:2]}, shift_amt_32, sign_ext_12_Itype, sign_ext_13, sign_ext_12_Stype, sign_ext_18,{15'b0,p0_instr[6:2],2'b0},    // inputs over
+all control signals, p1_pc, p1_instr_rs1, p1_instr_rs2, p1_rs1, p1_rs2, p1_rsc, p1_instr_rsc, p1_regdest, p1_regdestc1, p1_regdestc2, p1_shift_amt_32, p1_sign_ext_12_Itype, p1_sign_ext_13, p1_sign_ext_12_Stype, p1_sign_ext_18, p1_concat 
+ );
 
-forwarding forw (p2_regwr1, p2_regwr2, p1_rs1, p1_rs2, p1_rs3, p3_regwr1, p3_regwr2, p2_regdest1, p2_regdest2, p3_regdest1, p3_regdest2, p2_memwr1, p2_memwr2, p3_memwr1, p3_memwr2);
+
+mux8to1_32bit Forward1  (p1_rs1, p2_aluout, p2_adderout, regdata, regdatac, alu_in1);   // some_out1 some_out2
+mux8to1_32bit Forward2  (p1_rs2, p2_aluout, p2_adderout, regdata, regdatac, alu_in2_temp);
+mux8to1_32bit Forward3  (p1_rsc, p2_aluout, p2_adderout, regdata, regdatac, adder_in_1);
+
+mux8to1_32bit  lastmuxbeforealu (alu_in2_temp, p1_shift_amt_32, sign_ext_12_Itype, p1_sign_ext_12_Stype, p1_sign_ext_13, 32'b0, 32'b0, 32'b0, alusrcb, alu_in2);
+      // the last mux before adder uses branch address and CLW instruction ka 2nd input
 
 
-alu ALU_call(alu_in1, aluIn2, p1_aluOp, p1_negativeFlag, p1_carryFlag, p1_overflowFlag, p1_zeroFlag, p1_flagWrite, aluOut1, alu_N/P_flag); 
+mux2to1_32bit destinationcompressed (p1_regdestc1, p1_regdestc2, regdestc, p1_regdestc);
 
-mux2to1 sltimux (32'b0, 32'b1, alu_N/P_flag, slti_out);					// check if N/p flag is set when it is less or great
+// forwarding forw (p2_regwr1, p2_regwr2, p1_rs1, p1_rs2, p1_rs3, p3_regwr1, p3_regwr2, p2_regdest1, p2_regdest2, p3_regdest1, p3_regdest2, p2_memwr1, p2_memwr2, p3_memwr1, p3_memwr2);
+
+alu ALU_call(alu_in1, aluIn2, p1_aluOp, p1_overflowFlag, p1_carryFlag, p1_negFlag,p1_zeroFlag,Npflag,aluOut); 
+
+mux2to1 sltimux (32'b0, 32'b1, Npflag, slti_out);         // check if N/p flag is set when it is less or great
 mux2to1 alufinaloutput (aluOut1, slti_out, slti, aluout);
 mux2to1 compressed_1st_input (p1_pc, p1_rsc, control , adder_in_1);
 mux2to1 compressed_2nd_input (p1_concat, p1_branch_address, adder_in_2);
 
-adder32 Adder_instr2_call(adder_in_1, adder_in_2,  adderOut_instr2);
+adder32 Adder_instr2_call(adder_in_1, p1_concat,  adder_out);
 
 
 
 
-mux2to1 reg_dest_c (p1_regdestc1, p1_regdestc2, control, p1_regdestc);
+mux2to1_5bit reg_dest_c (p1_regdestc1, p1_regdestc2, control, p1_regdestc);
+mux2to1_32bit alu_or_slti (aluOut, slti_out, slti, aluout); 
 
-EX_MEM ex_mem(clk, reset, 1'b1, 1'b1, p1_regWrite1, p1_regWrite2, p1_memRead, p1_memWrite,
-  adder_out, aluOut,   p1_regdest, p1_regdestc, p1_rs2, p1_sign_ext_18,
-	p2_regWrite1, p2_regWrite2, p2_memRead, p2_memWrite, 
-	p2_adderOut, p2_AluOut, p2_regdest1, p2_regdest2, p2_rs2, p2_sign_ext_18,  n/pflag, p2_n/pflag, p1_reg);
 
-  
-  //Data Mem
+EX_MEM ex_mem(clk, reset, 1'b1, 1'b1, p1_regwrite, p1_regwritec, p1_memread, p1_memwrite,
+  adder_out, aluout,   p1_regdest, p1_regdestc, p1_rs2, p1_sign_ext_18, p1_pc,
+  p2_regwrite, p2_regwritec, p2_memread, p2_memwrite, 
+  p2_adderOut, p2_AluOut, p2_regdest, p2_regdestc, p2_rs2, p2_sign_ext_18, p2_pc);
 
-  mux2to1 slti (32'b0, 32'b1, p2_n/pflag, sltiout);
-
+ 
   wire [31:0] memOut;
   DataMem Data_mem(clk, reset, p2_memWrite, p2_memRead, p2_adderOut, p2_aluout, p2_rs2, memOut );
   
   
   
-  MEM_WB mem_wb( clk, reset, 1'b1, 1'b1, p2_memWrite, p2_regWrite1, p2_regWrite2,
-  p2_Overflow, p2_pc,
+  MEM_WB mem_wb( clk, reset, 1'b1, 1'b1, p2_memwrite, p2_regwrite, p2_regwritec,
+  p2_pc,
   memOut, p2_AluOut, 
-  p2_regdest, p2_regdestc, p2_sign_ext_18, sltiout,
+  p2_regdest, p2_regdestc, p2_sign_ext_18,
   p3_memWrite, p3_regWrite1, p3_regWrite2, 
-  p3_overflow, p3_pc,
+  p3_pc,
   p3_memOut, p3_aluOut, 
-  p3_sign_ext_18,
-  p3_sltiout,
-  p3_regdest, p3_regdestc);
+  p3_regdest, p3_regdestc, p3_sign_ext_18);
 
-  mux4to1 normalregdata (p3_pc, p3_aluout, p3_slti, 32'b0, control, regdata);
-  mux2to1 compressedregdata (  p3_sign_ext_18, p3_memout, control, regdatac);
+ 
+  mux2to1_32bit normalregdata (p3_aluout, p3_pc, memtoreg, regdata);
+  mux2to1_32bit compressedregdata (  p3_sign_ext_18, p3_memout, memtoregc, regdatac);
 
 
   
-  Forwarding fwd_ckt(p2_regwr1, p2_regwr2, p0_instr[35:31], p0_instr[40:36], p0_instr[9:7], p3_regwr1, p3_regwr2, p3_regdest, p3_regdestc, p3_memwr, p2_regdest, p2_regdestc,  
-   cntr1, cntrl2);
+  Forwarding fwd_ckt(1'b0, jump, branch, p2_regwrite, p2_regwritec, p0_instr[35:31], p0_instr[40:36], p0_instr[9:7], p3_regwrite, p3_regwritec, p3_regdest, p3_regdestc, p3_memwrite, p2_regdest, p2_regdestc,  
+  forward1, forward2, forward3, forward4, forward5);
 
+  Forwarding fwd_ckt1 (1'b1, 1'b0, 1'b0, p2_regwrite, p2_regwritec, p1_rs1, p1_rs2, p1_rsc, p3_regwrite, p3_regwritec, p3_regdest, p3_regdestc, p3_memwrite, p2_regdest, p2_regdestc,  
+  forward1, forward2, forward3, forward4, forward5)
 
-hazard hazard_call(p1_memread, p1_regwrite, p1_regdestc,  p0_instr[35:31], p0_instr[40:36], p0_instr[9:7], id_ex_control_value, pc_write, if_id_write);
-
-	
-	wire NEGATIVE, ZERO, CARRY;
-	
-	register1bit_flag Overflow_reg( clk, reset, p3_VFlagWrite, 1'b1, p3_overflow, OVERFLOW );
-	register1bit_flag Negative_reg( clk, reset, p3_NFlagWrite, 1'b1, p3_neg, NEGATIVE );
-	register1bit_flag Zero_reg( clk, reset, p3_ZFlagWrite, 1'b1, p3_zero, ZERO );
-	register1bit_flag Carry_reg( clk, reset, p3_CFlagWrite, 1'b1, p3_carry, CARRY );
-					
+  hazard hazard_call(jump, branch, p1_memread, p1_regwrite, p2_memread, p2_regwrite, p2_regdestc,p0_instr[35:31], p0_instr[40:36], p0_instr[9:7], hd_stall, pc_write, if_id_write);
+  
+   
+          
  assign A = p3_memOut;
  assign B = p3_aluOut;
  
